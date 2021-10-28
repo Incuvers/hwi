3cce82145a1a680d927dcd6bc55dd56e9fb910f4
cSDes1gn <christian911@sympatico.ca>
2021-10-28 18:37:02 -0400

    remove ghcr login

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -9,7 +9,7 @@ handler () {
     printf "%b" "${FAIL} ✗ ${NC} ${0##*/} failed on line $2 with exit status $1\n"
 }
 
-echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME" --password-stdin
+# echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME" --password-stdin
 
 printf "%b" "${OKB}Building project${NC}\n"
 docker compose -f "$DOCKER_ROOT"/docker-compose.yaml up
