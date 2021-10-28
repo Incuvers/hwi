e29582a5a9251031f3305d8cc5f7ddff7592c3b8
cSDes1gn <christian911@sympatico.ca>
2021-10-28 18:36:45 -0400

    spin up test rmq client

--- hwi/__main__.py
+++ hwi/__main__.py
@@ -9,6 +9,8 @@ import os
 import logging
 import coloredlogs
 
+from hwi.rmq.client import RMQClient
+
 _log = logging.getLogger(__name__)
 
 coloredlogs.install(level="DEBUG", logger=_log)
@@ -16,3 +18,6 @@ coloredlogs.install(level="DEBUG", logger=_log)
 # RMQ Event config
 host = os.environ['RABBITMQ_ADDR'].split(':')[0]
 port = int(os.environ['RABBITMQ_ADDR'].split(':')[1])
+
+rmq_client = RMQClient(host, port)
+rmq_client.publish()
