65a8a572d1c0c8e34c1793757b2a110262897159
cSDes1gn <christian911@sympatico.ca>
2021-10-28 18:37:09 -0400

    basic rmq publish

--- hwi/rmq/client.py
+++ hwi/rmq/client.py
@@ -6,10 +6,14 @@ Modified: 2021-10
 """
 
 import pika
+import json
+import time
+import logging
 
 
 class RMQClient:
     def __init__(self, host: str, port: int) -> None:
+        self._logger = logging.getLogger(__name__)
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(
                 host=host,
@@ -17,3 +21,13 @@ class RMQClient:
             )
         )
         self.channel = connection.channel()
+        self.channel.queue_declare(queue='test')
+
+    def publish(self):
+        body = json.dumps({"msg": "hello world"})
+        while True:
+            self.channel.basic_publish(exchange='',
+                                       routing_key='test',
+                                       body=body)
+            self._logger.info("Sent %s", body)
+            time.sleep(1)
