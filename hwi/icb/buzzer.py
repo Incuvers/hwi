75d45861d54e54e48739e7e31baac18aed5d62da
cSDes1gn <christian911@sympatico.ca>
2021-10-28 14:38:29 -0400

    autofmt

--- hwi/icb/buzzer.py
+++ hwi/icb/buzzer.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #  Copyright (C) Incuvers, Inc - All Rights Reserved
 #  Unauthorized copying of this file, via any medium is strictly prohibited
@@ -7,7 +6,8 @@
 #
 
 # TODO: This should not be a class or Package
-import wiringpi
+import wiringpi  # type: ignore
+
 
 class Buzzer:
     def __init__(self):
@@ -18,21 +18,20 @@ class Buzzer:
 
     def __del__(self):
         # turn that god thing off
-        wiringpi.digitalWrite(self.pin , 0)
+        wiringpi.digitalWrite(self.pin, 0)
 
     def beep(self):
         for _ in range(3):
-            wiringpi.digitalWrite(self.pin , 1)
+            wiringpi.digitalWrite(self.pin, 1)
             wiringpi.delay(150)
-            wiringpi.digitalWrite(self.pin , 0)
+            wiringpi.digitalWrite(self.pin, 0)
             wiringpi.delay(80)
 
     def off(self):
-        wiringpi.digitalWrite(self.pin , 0)
-
+        wiringpi.digitalWrite(self.pin, 0)
 
 
 if __name__ == "__main__":
-    buzzer=Buzzer()
+    buzzer = Buzzer()
     buzzer.beep()
     buzzer.off()
