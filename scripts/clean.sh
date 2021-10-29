08eb5754f6663c017c9ec835702eb513ae5c9d72
cSDes1gn <christian911@sympatico.ca>
2021-10-29 17:02:48 -0400

    lint

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,10 +7,10 @@ trap 'handler $? $LINENO' ERR
 
 handler () {
     printf "%b" "${FAIL} ✗ ${NC} ${0##*/} failed on line $2 with exit status $1\n"
-    exit $1
+    exit "$1"
 }
 
 printf "%b" "${OKB}Cleaning docker environment${NC}\n"
-docker-compose -f "$DOCKER_ROOT"/docker-compose.yaml down --rmi all --volumes;
+docker compose -f "$DOCKER_ROOT"/docker-compose.yaml down --rmi all --volumes;
 docker image prune --force;
 printf "%b" "${OKG} ✓ ${NC} complete\n"
