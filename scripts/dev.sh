cbec0183307877cd321792e7624c8e2770393c78
cSDes1gn <christian911@sympatico.ca>
2021-11-03 15:16:22 -0400

    auth

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,6 +2,7 @@
 
 source .env
 
+printf "%b" "${OKB}Authenticating user with github container registry${NC}\n"
 echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME" --password-stdin
 printf "%b" "${OKB}Building project${NC}\n"
 docker compose -f docker/dev/docker-compose.yaml up
