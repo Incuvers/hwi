df3c0926c30e2a6d839503bc4fedea922dbb7d2e
cSDes1gn <christian911@sympatico.ca>
2021-11-03 15:15:24 -0400

    uncomment login lins

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,7 +2,6 @@
 
 source .env
 
-# echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME" --password-stdin
-
+echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME" --password-stdin
 printf "%b" "${OKB}Building project${NC}\n"
 docker compose -f docker/dev/docker-compose.yaml up
