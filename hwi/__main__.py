dc440fa380f7bdc4949d81eb18428d1ed9bd575e
cSDes1gn <christian@incuvers.com>
2021-12-10 19:10:14 +0000

    add default log level handle

--- hwi/__main__.py
+++ hwi/__main__.py
@@ -11,7 +11,6 @@ import sys
 import os
 import logging
 import logging.config
-import coloredlogs
 from pathlib import Path
 from envyaml import EnvYAML
 from configparser import ConfigParser
@@ -31,6 +30,9 @@ def logging_handler(config_path: Path, base_path: str) -> None:
     :param base_path: logging path
     :type base_path: str
     """
+    # set default log level if undef
+    if not os.environ.get('HWI_LOG_LEVEL'):
+        os.environ['HWI_LOG_LEVEL'] = "INFO"
     os.makedirs(base_path, mode=0o777, exist_ok=True)
     # using split '.' to remove logs for rolling file handlers with format: <name>.log.<number>
     logs = list(
@@ -77,7 +79,6 @@ def device_certs_handler(base_path: str) -> None:
     os.environ['AMQP_PASS'] = config.get('amqp', 'password')
 
 _log = logging.getLogger(__name__)
-coloredlogs.install(level="DEBUG", logger=_log)
 _log.info("HWI Logs: %s", os.environ.get("HWI_LOGS"))
 _log.info("HWI Certs: %s", os.environ.get("HWI_CERTS"))
 
